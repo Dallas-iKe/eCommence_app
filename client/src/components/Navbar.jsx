26e0a1036bab34a227f2240c1e9a6f1cccd8d203
Isaac Mensah <clarity471@gmail.com>
2025-01-17 13:49:38 +0000

    New Commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,9 @@ const Navbar = () => {
 
     <div className='flex items-center justify-between py-5 font-medium'>
 
+      <Link to='/'>
       <img src={assets.logo} className='w-36' alt="logo" />
+      </Link>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
