d28048892f43535e8ab65966b801d1f74c8ec642
Isaac Mensah <clarity471@gmail.com>
2025-01-21 15:41:25 +0000

    New Commit

--- client/src/components/ProductItem.jsx
+++ client/src/components/ProductItem.jsx
@@ -10,7 +10,7 @@ const ProductItem = ({id, image,name,price}) => {
     <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
         <div className='overflow-hidden'>
             <img src={image[0]} alt={name} className='hover:scale-110 transition ease-in-out' />
-            <p className='pt-3 pb-1 text-[12.5px]'>{name}</p>
+            <p className='pt-3 pb-1 text-[12px]'>{name}</p>
             <p className='text-sm font-medium'>{currency}{price}</p>
         </div>
       
